b0711a54f0a12f8e5b6f9de3e496f4ba07b69d9f
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-08-25 20:50:45 -0500

    added Spotify Auth and Token and Search calls

---
+++ api/spotifyData.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import spotifyAuth from '../utils/spotifyAuth';
+
+const Spotify = () => new Promise((resolve, reject) => {
+  axios('https://accounts.spotify.com/api/token', {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${btoa(`${spotifyAuth.clientId}:${spotifyAuth.clientSecret}`)}`,
+    },
+    data: 'grant_type=client_credentials',
+    method: 'POST',
+  }).then((response) => resolve(response.data.access_token))
+    .catch((error) => reject(error));
+});
+
+const spotifySearch = (token, albumTitle) => new Promise((resolve, reject) => {
+  axios.get(`https://api.spotify.com/v1/search?q=${albumTitle}&type=album`, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    const albumObject = response.data.albums.items.filter((album) => album.name === `${albumTitle}` && album.album_type === 'album');
+    resolve(albumObject.shift());
+  }).catch((error) => reject(error));
+});
+
+export { Spotify, spotifySearch };
---
+++ utils/spotifyAuth.js
@@ -0,0 +1,6 @@
+const spotifyAuth = {
+  clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
+};
+
+export default spotifyAuth;
