fa95f1b4566ca7e661287075df74e8992dcdf765
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-08-27 09:20:31 -0500

    created ShowCard component

---
+++ components/ShowCard.js
@@ -0,0 +1,31 @@
+import { Card } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+function ShowCard({ showObject }) {
+  return (
+    <div className="showcard">
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            {showObject.starts_at}
+          </Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{showObject.venue.name}</Card.Subtitle>
+          <Card.Text>
+            {showObject.venue.location}
+          </Card.Text>
+          <Card.Link href={showObject.offers[0].url}>Buy Tickets</Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+ShowCard.propTypes = {
+  showObject: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.array,
+  ]).isRequired,
+};
+
+export default ShowCard;
