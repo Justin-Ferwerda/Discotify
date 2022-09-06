a459f533c7b92d25b569ca67ea1820b363fa2836
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-09-06 10:10:21 -0500

    changed album search so if album is already owned you cannot save it to collection

--- pages/albumPreview/[spotifyId].js
+++ pages/albumPreview/[spotifyId].js
@@ -1,20 +1,37 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { getAlbums } from '../../api/albumData';
 import SpotifyPlayer from '../../components/SpotifyPlayer';
 
 function AlbumPreview() {
   const router = useRouter();
   const { spotifyId } = router.query;
+  const [albums, setAlbums] = useState([]);
 
   const handleClick = () => {
     router.push(`/album/saveToCollection/${spotifyId}`);
   };
 
+  const setState = () => {
+    getAlbums().then(setAlbums);
+  };
+
+  useEffect(() => {
+    setState();
+  }, []);
+
   return (
-    <div className="albumPreviewPage">
-      <SpotifyPlayer spotifyId={spotifyId} height={360} />
-      <Button onClick={handleClick}>Save To Collection</Button>
-    </div>
+    albums.some((album) => album.spotifyId === spotifyId) ? (
+      <div>
+        <h1>Sorry album is already owned, please pick another!</h1>
+      </div>
+    ) : (
+      <div className="albumPreviewPage">
+        <SpotifyPlayer spotifyId={spotifyId} height={360} />
+        <Button onClick={handleClick}>Save To Collection</Button>
+      </div>
+    )
 
   );
 }
